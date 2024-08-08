3591561fc2ad983f1c0e58446d4c78a5d773fde4
h2o-murakami <117067938+h2o-murakami@users.noreply.github.com>
2024-08-08 17:08:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from vertexai.preview.vision_models import ImageGenerationModel
   
 # 環境変数の設定
 PROJECT_ID = "tst-kpgs-poc-h2o"  # Google Cloud プロジェクトの ID
-LOCATION = "asia-northeast1"  # Gemini モデルを使用するリージョン
+LOCATION = "us-central1"  # Gemini モデルを使用するリージョン
   
 vertexai.init(project=PROJECT_ID, location=LOCATION)
   
